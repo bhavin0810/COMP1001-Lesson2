9e0e70b7bd06b6f31afbc88a1f9274c787026123
bhavin0810 <bhavin0810@gmail.com>
2015-01-15 15:32:56 -0500

    Lesson 2 Added

---
+++ COMP1011-Lesson2/src/Hero.java
@@ -0,0 +1,50 @@
+
+public class Hero {
+	
+	//Private Properties
+	private int strength;
+	private int speed;
+	private int health;
+	private String name;
+	
+	///Getters and Setters
+	public int getStrength() {
+		return strength;
+	}
+
+	public int getSpeed() {
+		return speed;
+	}
+
+	public int getHealth() {
+		return health;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	//constructor of the class
+	public Hero(){
+		this.setName(name);
+	}
+	
+	//Private Method
+	private void generateAbilities(){
+		
+	}
+	
+	public void fight(){
+		
+	}
+	
+	public void run(){
+		
+	}
+
+	
+}
---
+++ COMP1011-Lesson2/src/Project.java
@@ -0,0 +1,11 @@
+
+public class Project {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Hero a  = new Hero();
+		
+	}
+
+}
