eadb9647176753a8817475f60afa2c5c1f37dd4c
bhavin0810 <bhavin0810@gmail.com>
2015-01-15 15:46:11 -0500

    Updated Hero Class

--- COMP1011-Lesson2/src/Hero.java
+++ COMP1011-Lesson2/src/Hero.java
@@ -31,19 +31,33 @@ public class Hero {
 	//constructor of the class
 	public Hero(){
 		this.setName(name);
+		
+		generateAbilities();
+	}
+	
+	
+	//Public Methods
+	public void showAbilities(){
+		System.out.println("################################");
+		System.out.println("Strength: " + this.strength);
+		System.out.println("Speed: " + this.speed);
+		System.out.println("Health: " + this.health);
+		System.out.println("################################");
 	}
 	
 	//Private Method
 	private void generateAbilities(){
-		
+		this.strength = (int)(Math.random() * 100 + 1);
+		this.speed = (int)(Math.random() * 100 + 1);
+		this.health = (int)(Math.random() * 100 + 1);
 	}
 	
 	public void fight(){
-		
+		System.out.println( this.getName() + " is Fighting");		
 	}
 	
 	public void run(){
-		
+		System.out.println( this.getName() + " is Running");
 	}
 
 	
